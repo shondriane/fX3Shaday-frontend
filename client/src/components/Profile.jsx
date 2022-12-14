a8441d07ecb0c14f52cf7a56df270e4f63515281
Shondriane <shondriane.wise@yahoo.com>
2022-12-14 04:26:55 -0500

    working on useProfile styling

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -9,6 +9,22 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import AddCircleIcon from '@mui/icons-material/AddCircle'
 import ClearIcon from '@mui/icons-material/Clear';
 import '../index.css';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { CardActions, CardHeader } from '@mui/material';
+
+
 
 const Profile = ({ user,handleLogOut }) => {
 	let navigate = useNavigate();
@@ -45,52 +61,77 @@ const Profile = ({ user,handleLogOut }) => {
 	}, [user]);
 
 	return (
-		<div className="profile">
-			<section className="profileSection">
-				<h1>
-					{profile.firstName} {profile.lastName}
-				</h1>
-				<h2>Email</h2>
-				<h3>{profile.email}</h3>
-				<h2>Contact Number</h2>
-				<h3>{profile.phoneNumber}</h3>
+        <div >
+       <Grid
+  container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justify="center"
+  style={{ minHeight: '100vh' }}
+ >
+    
+            <Box width='400px' mt={4}>
+                
+            <Card>
+                <CardHeader sx={{ bgcolor: "#f0e5f4" }}/>
+                <CardContent>
+<Typography gutterButtom variant="h5" component="div">
+Name: {profile.firstName} {profile.lastName}
+</Typography>
+<Typography gutterButtom variant="h5" >
+Email: {profile.email}
+</Typography>
+<Typography gutterButtom variant="h5" >
+Phone: {profile.phoneNumber}
+</Typography>
+
+
+                </CardContent>
+                <CardActions>
                 <Button variant="contained" startIcon={<AddCircleIcon/>}onClick={handleUpdateClick}>
-							Update Profile
-						</Button>
-                        <Button variant="outlined" endIcon={<DeleteIcon/>} onClick={handleClick}>
-							Delete Profile
-						</Button>
+ 							Update Profile
+ 						</Button>
+                         <Button variant="outlined" endIcon={<DeleteIcon/>} onClick={handleClick}>
+ 							Delete Profile
+ 						</Button>
+                </CardActions>
+            </Card>
+ 			
 	
-				{ show &&(
-					 <div id ="id01" className="modal">
+ 				{ show &&(
+ 					 <div id ="id01" className="modal">
                     
-                     <form className="modal-content">
-                         <div className="container">
-<h1>
-Delete Account
-</h1>
-<p>
-Are you sure you want to delete your account
-</p>
+                      <form className="modal-content">
+                          <div className="container">
+ <h1>
+ Delete Account
+ </h1>
+ <p>
+ Are you sure you want to delete your account
+ </p>
                         
                          
-                   <div class="clearfix">
-                   <Button variant="contained" endIcon = {<ClearIcon/>}onClick={handleUpdateClick}>
-                         cancel
-                     </Button>
-                     <Button variant="outlined" startIcon={<DeleteIcon/>} onClick={handleDeleteClick}>
-                         Delete 
-                     </Button>
+                 <div class="clearfix">
+                 <Button variant="contained" endIcon = {<ClearIcon/>}onClick={handleUpdateClick}>
+                        cancel
+                      </Button>
+                      <Button variant="outlined" startIcon={<DeleteIcon/>} onClick={handleDeleteClick}>
+                          Delete 
+                      </Button>
                     
-                     </div>
-                 </div>
-                 </form>
-                 </div>
-                )}
+                      </div>
+                  </div>
+                  </form>
+                  </div>
+                 )}
                     	
 				
-			</section>
-		</div>
+ 		
+           
+             </Box>
+             </Grid>
+		</div> 
 	);
 };
 
