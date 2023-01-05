a0b475d54d7fb651e010687bfd92181565793cfa
Shondriane <shondriane.wise@yahoo.com>
2023-01-05 09:44:20 -0500

    adding condition

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import{useState} from 'react'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -15,7 +16,7 @@ const Review =({id,comment,rating,name})=>{
 
     return(
       <div >
-        {show && `${comment}`!==""(
+        {`${comment}`!==""(
         <Box  component ="span" sx={{display:'column', mx:'2px',justifyConent:"center", alignItems:"center"}}>
         <Grid  className="reviewRatings" container spacing={4} direction="flex" flexWrap="wrap"alignItems="center" justify="center">
           
